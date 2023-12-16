ac105218b717ab3ed1cfea3a61b23636908ffc8a
Stephan Hochdörfer <S.Hochdoerfer@bitExpert.de>
2023-12-16 15:28:57 +0100

    Simplify slider handling

--- src/Resources/public/range-slider.js
+++ src/Resources/public/range-slider.js
@@ -1,39 +1,45 @@
 $(document).ready(function() {
     var sliderElement = $('.range-slider');
-    var hiddenInput = $(sliderElement.attr('data-input-id'));
-
-    var min = parseInt(sliderElement.attr('data-min'));
-    var max = parseInt(sliderElement.attr('data-max'));
-    var valuesForSlider = [];
-    for (var i = min; i <= max; i++) {
-        valuesForSlider.push(i);
-    }
-
-    var start = [valuesForSlider[0], valuesForSlider[valuesForSlider.length - 1]];
-    var value = sliderElement.attr('data-value').split(";");
-    if (value.length === 2) {
-        start = value;
-    }
-
-    var slider = noUiSlider.create(sliderElement[0], {
-        start: start,
-        step: 1,
-        tooltips: true,
-        connect: true,
-        range: {
-            'min': 0,
-            'max': valuesForSlider.length - 1,
-        },
-        format: {
-            to: function (value) {
-                return valuesForSlider[Math.round(value)];
-            },
-            from: function (value) {
-                return valuesForSlider.indexOf(Number(value));
+    if (sliderElement.length > 0) {
+        var hiddenInput = $(sliderElement.attr('data-input-id'));
+        if (hiddenInput.length > 0) {
+            var min = parseInt(sliderElement.attr('data-min'));
+            var max = parseInt(sliderElement.attr('data-max'));
+            var valuesForSlider = [];
+            for (var i = min; i <= max; i++) {
+                valuesForSlider.push(i);
             }
+
+            var start = [valuesForSlider[0], valuesForSlider[valuesForSlider.length - 1]];
+            var value = sliderElement.attr('data-value');
+            if (value !== undefined) {
+                value = value.split(";");
+                if (value.length === 2) {
+                    start = value;
+                }
+            }
+
+            var slider = noUiSlider.create(sliderElement[0], {
+                start: start,
+                step: 1,
+                tooltips: true,
+                connect: true,
+                range: {
+                    'min': 0,
+                    'max': valuesForSlider.length - 1,
+                },
+                format: {
+                    to: function (value) {
+                        return valuesForSlider[Math.round(value)];
+                    },
+                    from: function (value) {
+                        return valuesForSlider.indexOf(Number(value));
+                    }
+                }
+            });
+            slider.on('end', function(values) {
+                hiddenInput.val(values[0] + ";" + values[1]);
+            });
         }
-    });
-    slider.on('end', function(values) {
-        hiddenInput.val(values[0] + ";" + values[1]);
-    });
+    }
 });
